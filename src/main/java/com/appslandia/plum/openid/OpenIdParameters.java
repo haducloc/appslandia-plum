bee86c95741046a2f19e4195c34a5e71a2fd5319
Loc Ha <loc.ha@nebraska.gov>
2023-02-21 13:33:36 -0600

    Add OpenIdParameters

--- src/main/java/com/appslandia/plum/openid/OpenIdParameters.java
+++ src/main/java/com/appslandia/plum/openid/OpenIdParameters.java
@@ -20,41 +20,32 @@
 
 package com.appslandia.plum.openid;
 
-import java.util.HashMap;
-
-import com.appslandia.common.base.MapWrapper;
-import com.appslandia.common.utils.Asserts;
-import com.appslandia.common.utils.STR;
-
 /**
  *
  * @author <a href="mailto:haducloc13@gmail.com">Loc Ha</a>
  *
  */
-public class OpenIdUser extends MapWrapper<String, Object> {
-    private static final long serialVersionUID = 1L;
+public class OpenIdParameters {
 
-    public static final String ID = "id";
-    public static final String SUB = "sub";
+    public static final String CLIENT_ID = "client_id";
+    public static final String CLIENT_SECRET = "client_secret";
 
-    public static final String FIRST_NAME = "first_name";
-    public static final String LAST_NAME = "last_name";
-    public static final String MIDDLE_NAME = "middle_name";
-    public static final String EMAIL = "email";
+    public static final String REDIRECT_URI = "redirect_uri";
 
-    public static final String LOCALE = "locale";
-    public static final String PICTURE = "picture";
+    public static final String STATE = "state";
+    public static final String NONCE = "nonce";
+    public static final String SCOPE = "scope";
 
-    public static final String NAME = "name";
-    public static final String FAMILY_NAME = "family_name";
-    public static final String GIVEN_NAME = "given_name";
+    public static final String DISPLAY = "display";
+    public static final String PROMPT = "prompt";
+    public static final String LOGIN_HINT = "login_hint";
 
-    public OpenIdUser() {
-	super(new HashMap<>());
-    }
+    public static final String CODE = "code";
+    public static final String ACCESS_TOKEN = "access_token";
+    public static final String INPUT_TOKEN = "input_token";
 
-    public Object getRequired(String key) {
-	Object obj = this.get(key);
-	return Asserts.notNull(obj, () -> STR.fmt("No value associated with key '{}'.", key));
-    }
+    public static final String AUTH_TYPE = "auth_type";
+    public static final String GRANT_TYPE = "grant_type";
+    public static final String ACCESS_TYPE = "access_type";
+    public static final String RESPONSE_TYPE = "response_type";
 }
