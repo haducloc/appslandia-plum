73725d560c42633e00b6f80e808026dcc5ca23fe
Loc Ha <loc.ha@nebraska.gov>
2023-08-08 09:21:53 -0500

    Add pebble custom functions

---
+++ src/main/java/com/appslandia/plum/pebble/functions/ActionUrlFunction.java
@@ -0,0 +1,54 @@
+// The MIT License (MIT)
+// Copyright © 2015 AppsLandia. All rights reserved.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+package com.appslandia.plum.pebble.functions;
+
+import java.util.Map;
+
+import com.appslandia.plum.base.ActionParser;
+import com.appslandia.plum.pebble.DynPebbleFunction;
+import com.appslandia.plum.pebble.TemplateEvaluationContext;
+import com.appslandia.plum.utils.ServletUtils;
+
+/**
+ *
+ * @author <a href="mailto:haducloc13@gmail.com">Loc Ha</a>
+ *
+ */
+public class ActionUrlFunction extends DynPebbleFunction {
+
+    @Override
+    protected Object doExecute(TemplateEvaluationContext context, int lineNumber) {
+	String action = context.getRequiredArgument("action");
+
+	String controller = context.getArgument("controller");
+	if (controller == null) {
+	    controller = context.getRequestContext().getActionDesc().getController();
+	}
+
+	boolean absolute = context.getBool("absolute", false);
+	Map<String, Object> parameters = context.parseParameters();
+
+	ActionParser actionParser = ServletUtils.getAppScoped(context.getRequest().getServletContext(), ActionParser.class);
+	String url = actionParser.toActionUrl(context.getRequest(), controller, action, parameters, absolute);
+
+	return context.getResponse().encodeURL(url);
+    }
+}
---
+++ src/main/java/com/appslandia/plum/pebble/functions/MailtoFunction.java
@@ -0,0 +1,65 @@
+// The MIT License (MIT)
+// Copyright © 2015 AppsLandia. All rights reserved.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+package com.appslandia.plum.pebble.functions;
+
+import java.io.IOException;
+
+import com.appslandia.common.utils.StringUtils;
+import com.appslandia.common.utils.URLEncoding;
+import com.appslandia.plum.pebble.DynPebbleFunction;
+import com.appslandia.plum.pebble.TemplateEvaluationContext;
+
+/**
+ *
+ * @author <a href="mailto:haducloc13@gmail.com">Loc Ha</a>
+ *
+ */
+public class MailtoFunction extends DynPebbleFunction {
+
+    @Override
+    protected Object doExecute(TemplateEvaluationContext context, int lineNumber) throws IOException {
+	String to = context.getRequiredArgument("to");
+	String cc = context.getArgument("cc");
+	String bcc = context.getArgument("bcc");
+
+	String subject = context.getArgument("subject");
+	String body = context.getArgument("body");
+
+	StringBuilder mailto = new StringBuilder(256);
+
+	mailto.append("mailto:").append(to);
+	mailto.append("?t=").append(System.currentTimeMillis());
+
+	if (!StringUtils.isNullOrEmpty(cc)) {
+	    mailto.append("&cc=").append(cc);
+	}
+	if (!StringUtils.isNullOrEmpty(bcc)) {
+	    mailto.append("&bcc=").append(bcc);
+	}
+	if (!StringUtils.isNullOrEmpty(subject)) {
+	    mailto.append("&subject=").append(URLEncoding.encodeParam(subject, false));
+	}
+	if (!StringUtils.isNullOrEmpty(body)) {
+	    mailto.append("&body=").append(URLEncoding.encodeParam(body, false));
+	}
+	return mailto.toString();
+    }
+}
