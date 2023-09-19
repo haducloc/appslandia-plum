48dc32bd0866fb31285983b583b748d3dee874fa
Loc Ha <loc.ha@nebraska.gov>
2023-09-19 13:30:12 -0500

    Minor fixes

--- src/main/java/com/appslandia/plum/base/StringFormatProvider.java
+++ src/main/java/com/appslandia/plum/base/StringFormatProvider.java
@@ -24,8 +24,8 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.appslandia.common.base.StringFormat;
 import com.appslandia.common.base.InitializeObject;
+import com.appslandia.common.base.StringFormat;
 import com.appslandia.common.utils.Asserts;
 
 /**
